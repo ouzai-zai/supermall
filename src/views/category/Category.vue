377a919ca3bb02d0b448c83c0e0d3087f338c69f
ouzai-zai <w03122@qq.com>
2021-09-28 16:56:55 +0800

    牛啊

---
+++ src/views/category/Category.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>分类</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"Category",
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    methods: {}
+  }
+</script>
+<style lang='css' scoped>
+
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>我的</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"Profile",
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    methods: {}
+  }
+</script>
+<style lang='css' scoped>
+
+</style>

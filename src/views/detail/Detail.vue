eb81e5996e5563451962972ebc7a454d6b51e6c1
ouzai-zai <w03122@qq.com>
2021-10-09 11:27:03 +0800

    2021.10.9

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,26 +1,52 @@
 <template>
   <div id="dtail">
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images='topImages'></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-shop-info :shop='shop'></detail-shop-info>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar.vue'
+  import DetailSwiper from './childComps/DetailSwiper.vue'
+  import DetailBaseInfo from './childComps/DetailBaseInfo.vue'
+  import DetailShopInfo from './childComps/DetailShopInfo.vue'
+
+  import {getDetail, Goods, Shop} from 'network/detail'
 
   export default {
     name:"Detail",
     data () {
       return {
-        iid: null
+        iid: null,
+        topImages: [],
+        goods: {},
+        shop: {}
       };
     },
     created () {
       // 保存传入的iid
       this.iid = this.$route.params.iid
+
+      // 根据iid进行数据请求
+      getDetail(this.iid).then(res => {
+        // 获取顶部图片轮播图数据
+        console.log(res);
+        const data = res.result
+        this.topImages = data.itemInfo.topImages
+        // 获取商品数据
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+        // 创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
+      })
     },
 
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo
     },
 
     computed: {},
